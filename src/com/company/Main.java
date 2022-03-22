236de3ace3eb1fd3982d66281f9d294c5b1b697d
Arlen002 <kurbanbaevarlen001@gmail.com>
2022-03-22 10:26:58 +0300

    tab-27

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
- 
-     Box.method();
-
+         Box.method();
     }
 }
